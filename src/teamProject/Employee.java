95c388bece663826ab2f301eff7a958ee3f24b10
Akvareil <andrej.shablovskij@gmail.com>
2015-01-31 17:07:48 +0200

    Add Employee
    Generate and add Employee class

---
+++ src/teamProject/Employee.java
@@ -0,0 +1,92 @@
+package myProject;
+
+import java.util.Date;
+
+public class Employee {
+	String id;
+	String firstName;
+	String lastName;
+	String email;
+	String phone;
+	Date hireDate;
+	String jobId;
+	Double salary;
+
+	public Employee(String id, String firstName, String lastName, String email,
+			String phone, Date hireDate, String jobId, Double salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.hireDate = hireDate;
+		this.jobId = jobId;
+		this.salary = salary;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
