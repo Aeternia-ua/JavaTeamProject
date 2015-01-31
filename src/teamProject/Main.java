2e6445d16ec551d27444afe94eeafd3abe91c9a8
AndrewBil <andrew.bilogan@gmail.com>
2015-01-31 16:29:01 +0200

    Create project

---
+++ src/teamProject/Main.java
@@ -0,0 +1,12 @@
+package teamProject;
+
+public final class Main {
+
+	public static void main(String[] args) {
+		Process.read();
+		Process.sort();
+		Process.print();
+		Process.write();
+	}
+
+}
